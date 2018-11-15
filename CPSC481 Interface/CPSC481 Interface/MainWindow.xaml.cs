8cc5cd31aa61db421034fd951cb94ac27c8da51d
davidnguyen2 <david.nguyen2@ucalgary.ca>
2018-11-14 17:37:03 -0700

    Implemented search
    Search with filler options

--- CPSC481 Interface/CPSC481 Interface/MainWindow.xaml.cs
+++ CPSC481 Interface/CPSC481 Interface/MainWindow.xaml.cs
@@ -34,5 +34,96 @@ namespace CPSC481_Interface {
 				SearchBox.Text = "Search Course";
 			}
 		}
-	}
+
+        // Data for courses to search from
+        static public List<string> GetData() {
+            List<string> data = new List<string>();
+            data.Add("CPSC 231");
+            data.Add("MATH 211");
+            data.Add("CPSC 481");
+            data.Add("LING 201");
+            data.Add("CPSC 413");
+            data.Add("PHIL 314");
+
+            return data;
+        }
+
+
+        private void SearchBox_KeyUp(object sender, KeyEventArgs e) { 
+        
+
+            bool found = false;
+            var border = (resultStack.Parent as ScrollViewer).Parent as Border;
+            var data = MainWindow.GetData();
+
+            string query = (sender as TextBox).Text;
+
+            if (query.Length == 0)
+            {
+                // Clear   
+                resultStack.Children.Clear();
+                border.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                border.Visibility = System.Windows.Visibility.Visible;
+            }
+
+            // Clear the list   
+            resultStack.Children.Clear();
+
+            // Add the result   
+            foreach (var obj in data)
+            {
+                if (obj.ToLower().StartsWith(query.ToLower()))
+                {
+                    // The word starts with this... Autocomplete must work   
+                    addItem(obj);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                resultStack.Children.Add(new TextBlock() { Text = "No results found." });
+            }
+        }
+
+        private void addItem(string text)
+        {
+            TextBlock block = new TextBlock();
+
+
+            // Add the text   
+            block.Text = text;
+            
+
+
+            // A little style...   
+            block.Margin = new Thickness(2, 3, 2, 3);
+            block.Cursor = Cursors.Hand;
+
+            // Mouse events   
+            block.MouseLeftButtonUp += (sender, e) =>
+            {
+                SearchBox.Text = (sender as TextBlock).Text;
+            };
+
+            block.MouseEnter += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.PeachPuff;
+            };
+
+            block.MouseLeave += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.Transparent;
+            };
+
+            // Add to the panel   
+            resultStack.Children.Add(block);
+        }
+
+    }
 }
